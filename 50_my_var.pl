2beb4c12e516d7a1191f77e2a2796f1f64f85040
Kondepu Nagarjuna <109850570+nagarjuna17444@users.noreply.github.com>
2023-03-26 17:48:13 +0530

    Add files via upload

---
+++ 50_my_var.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+#
+#
+#
+ $var=500;
+
+
+ sub test
+ {
+
+	 my $var=50;
+	 print "value of variable inside subroutine:$var\n";
+ }
+
+ test();
+
+ print "value of variable outside the subroutine:$var\n";
+
